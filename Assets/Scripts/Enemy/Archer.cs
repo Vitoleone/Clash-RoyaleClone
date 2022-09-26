14be67b7e9adf5e3bcb1bb5a070f060badc333b4
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-09-26 20:03:35 +0300

    Archer added

---
+++ Assets/Scripts/Enemy/Archer.cs
@@ -0,0 +1,85 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Archer : MonoBehaviour,IEnemy
+{
+    //Castle objects
+    public GameObject castle;
+    Castle castleInstance;
+    //Enemy Attributes
+    [SerializeField] float health = 150f;
+    float damage = 35f;
+    float speed = 4.5f;
+    float attackRate = 1.25f;
+    //Other components
+    NavMeshAgent navMeshAgent;
+    [SerializeField]GameObject Arrow;
+
+    private void Awake()
+    {
+        castleInstance = castle.GetComponent<Castle>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    void Start()
+    {
+        Move();
+
+    }
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        CheckRange(transform.position, 3.5f);
+        if (navMeshAgent.velocity == Vector3.zero)
+        {
+            InvokeRepeating("Attack", attackRate, attackRate);
+        }
+    }
+
+
+    public void Attack()
+    {
+        if (castle != null)
+        {
+            GameObject arrow = Instantiate(Arrow, transform.position, Quaternion.identity);
+            arrow.transform.DOMove(castle.transform.position, 0.5f);
+        }
+        CancelInvoke();
+    }
+
+    public void Move()
+    {
+        navMeshAgent.SetDestination(castle.transform.position);
+        navMeshAgent.speed = speed;
+    }
+    public void CheckRange(Vector3 center, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        List<Collider> collidersList = hitColliders.ToList();
+        foreach (var hitCollider in collidersList)
+        {
+            if (hitCollider != null)
+            {
+                if (hitCollider.gameObject.name == "Castle")
+                {
+                    navMeshAgent.velocity = Vector3.zero;
+                }
+            }
+            else
+            {
+                collidersList.Remove(hitCollider);
+            }
+        }
+    }
+
+    public void GetHit(float damage)
+    {
+        health -= damage;
+    }
+}
