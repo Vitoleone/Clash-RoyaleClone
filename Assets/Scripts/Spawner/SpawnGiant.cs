35fa644efce2cf2cbc6685bb59fa10f3a9dba4e8
Noyan Berkay Aşkın <berkay4.askin@hotmail.com>
2022-09-28 11:12:17 +0300

    can spawn giant with mouse drag and drop

--- Assets/Scripts/Spawner/SpawnGiant.cs
+++ Assets/Scripts/Spawner/SpawnGiant.cs
@@ -1,15 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 public class SpawnGiant : MonoBehaviour
 {
     public GameObject giant;
-    [SerializeField] GameObject SpawnPoint;
-    GameObject newGiant;
-    public void Spawn()
+    public GameObject newgiant;
+    bool canSpawn = true;
+    private Vector3 mOffset;
+    private float mZCoord;
+
+    private void OnMouseDown()
     {
-        newGiant = Instantiate(giant,SpawnPoint.transform.position,Quaternion.identity);
-        
+        if(canSpawn)
+        {
+            newgiant = Instantiate(giant,transform);
+            newgiant.GetComponent<GiantAllie>().enabled = false;
+            newgiant.GetComponent<NavMeshAgent>().enabled = false;
+            newgiant.GetComponent<Animator>().enabled = false;
+            canSpawn = false;
+        }
+        mZCoord = Camera.main.WorldToScreenPoint(newgiant.transform.position).z;
+        mOffset = newgiant.transform.position - GetMouseWorldPos();
     }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+    private void OnMouseDrag()
+    {
+        newgiant.transform.position = GetMouseWorldPos() + mOffset;
+    }
+    private void OnMouseUp()
+    {
+        newgiant.transform.position = GetMouseWorldPos() + mOffset;
+        newgiant.GetComponent<GiantAllie>().enabled = true;
+        newgiant.GetComponent<NavMeshAgent>().enabled = true;
+        newgiant.GetComponent<Animator>().enabled = true;
+        canSpawn = true;
+    }
+
 }
